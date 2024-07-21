aef8daab1f2d5876b43b175d0c41c1921256ff17
fgbyte <fer.garciabayon@gmail.com>
2024-07-21 09:55:44 -0400

    shadcn works

---
+++ app/components/app-logo.tsx
@@ -0,0 +1,4 @@
+const AppLogo = () => (
+	<img className="h-10 w-10" src="/favicon.png" alt="logo" />
+);
+export default AppLogo;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,19 @@
 import type { MetaFunction } from "@remix-run/node";
+import AppLogo from "~/components/app-logo";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "New Remix App" },
+		{ name: "description", content: "Welcome to Remix!" },
+	];
 };
 
 export default function Index() {
-  return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<section className="w-full bg-white min-h-screen flex flex-col">
+			<nav className="flex items-center space-x-2 px-4">
+				<AppLogo className="h-8 w-8 md:h-10 md:w-10" />
+			</nav>
+		</section>
+	);
 }
